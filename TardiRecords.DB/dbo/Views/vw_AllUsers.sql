464a2cde084cd06d9e3a2e54f04f359c70a717a6
Hristijan Stojanoski <hristijan.stojnoski@evolve-is.com.au>
2018-07-31 17:13:31 +0200

    DB project update

---
+++ TardiRecords.DB/dbo/Views/vw_AllUsers.sql
@@ -0,0 +1,5 @@
+﻿
+CREATE VIEW [dbo].[vw_AllUsers]
+AS
+SELECT NU.Id AS AspNetUserId, AU.id AS AppUserId, NU.Email, AU.firstName, AU.lastName
+FROM AspNetUsers NU INNER JOIN AppUsers AU ON NU.Id=AU.aspNetUserId
